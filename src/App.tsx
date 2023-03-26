34956ffa80d3458db5aac4ddc2deb5498ef3b96b
Anshita14 <55407072+Anshita14@users.noreply.github.com>
2023-03-26 20:28:52 +0530

    expenseTrackerApp

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import ExpenseTracker from './Components/ExpenseTRacker';
+import ShowData from './Components/ShowList';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/add" element={<ExpenseTracker onTrue={undefined} onClose={undefined} />} />
+          <Route path="/" element={<ShowData />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
